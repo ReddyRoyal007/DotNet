98556e192b2bbfb806c482b6140ba0cf78f9c8eb
ReddyRoyal007 <reddappa.govindu@gmail.com>
2024-08-09 21:02:36 +0530

    Add project files.

---
+++ AspNetMvcCoreProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspNetMvcCoreProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
